4245c08392edba57911c9875f6fb4c2f05ca07a5
Rsaldana89 <ciberboss@outlook.com>
2023-05-15 13:20:14 -0600

    first ApagadoTime

---
+++ README.md
@@ -0,0 +1,36 @@
+ApagadoTime
+
+ApagadoTime is a Python application that allows you to schedule the shutdown of your device. You can specify the time at which you want your computer to automatically shut down.
+
+Prerequisites
+
+Python 3.x installed on your system. You can download it from python.org.
+Tkinter library. If you don't have Tkinter installed, you can install it using the following command:
+Copy code
+pip install tkinter
+Usage Instructions
+Clone or download this repository to your computer.
+
+Open a terminal and navigate to the location of the "apagado.py" file.
+
+Run the following command to start the application:
+
+Copy code
+python apagado.py
+The ApagadoTime application window will appear.
+
+Click the "INGRESA EN CUANTO TIEMPO DEBE APAGARSE LA PC" button to display the configuration options.
+
+Enter the number of hours and minutes in the respective fields.
+
+Click the "Apagar" button to schedule the shutdown of your device.
+
+To cancel the scheduled shutdown, click the "Cancelar Apagado" button.
+
+The "TAMBIEN PUEDES CANCELAR EL APAGADO" label will display a confirmation message when the shutdown is canceled.
+
+Contributions
+Contributions are welcome. If you want to improve this application or add new features, feel free to fork this repository and submit your pull requests.
+
+License
+This project is licensed under the MIT License.
---
+++ apagadotime.py
@@ -0,0 +1,77 @@
+import tkinter as tk
+import os
+import ctypes
+import subprocess
+import sys
+
+# Verificar si el script se está ejecutando en una consola de comandos o directamente
+if hasattr(sys, 'frozen'):
+    # Establecer la ventana principal de la aplicación como oculta
+    ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 0)
+
+def mostrar_mensaje():
+    etiqueta.config(text="TAMBIEN PUEDES CANCELAR EL APAGADO")
+    etiqueta.config(fg="white")
+
+
+ventana = tk.Tk()
+ventana.title("ApagadoTime")
+ventana.geometry("400x500")
+ventana.configure(bg="#333333")
+
+fuente_grande2 = ("Arial", 18)
+fuente_grande = ("Arial", 14)
+
+boton = tk.Button(ventana, text="INGRESA EN CUANTO TIEMPO DEBE APAGARSE LA PC", command=mostrar_mensaje)
+boton.pack(pady=10)
+
+etiqueta = tk.Label(ventana, text="")
+etiqueta.configure(bg="#333333")
+etiqueta.pack(pady=10)
+
+
+def apagar_equipo():
+    horas = entry_horas.get()
+    minutos = entry_minutos.get()
+    tiempo_apagado = f"{horas}:{minutos}:00"
+    
+    segundos = int(horas) * 3600 + int(minutos) * 60
+
+    # Ejecutar el comando de apagado con el tiempo especificado en segundos
+    os.system(f"shutdown -s -t {segundos}")
+
+
+
+def cancelar_apagado():
+    os.system("shutdown -a")
+    etiqueta_mensaje.config(text="El apagado ha sido cancelado")
+
+etiqueta_mensaje = tk.Label(ventana, text="", font=fuente_grande)
+etiqueta_mensaje.configure(bg="#333333")
+etiqueta_mensaje.pack(pady=10)
+
+
+label_horas = tk.Label(ventana, text="Horas:", font=fuente_grande)
+label_horas.pack()
+
+entry_horas = tk.Entry(ventana)
+entry_horas.insert(0, "0")  # Establecer el valor predeterminado en "0"
+entry_horas.pack()
+
+label_minutos = tk.Label(ventana, text="Minutos:", font=fuente_grande)
+label_minutos.pack()
+
+entry_minutos = tk.Entry(ventana)
+entry_minutos.insert(0, "15")  # Establecer el valor predeterminado en "15"
+entry_minutos.pack()
+
+boton_apagar = tk.Button(ventana, text="Apagar", font=fuente_grande2, command=apagar_equipo)
+boton_apagar.pack()
+
+boton_cancelar = tk.Button(ventana, text="Cancelar Apagado", command=cancelar_apagado, font=fuente_grande)
+boton_cancelar.pack()
+
+
+
+
+ventana.mainloop()
